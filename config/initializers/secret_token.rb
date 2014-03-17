b98d917a4ed65c324bf3efd375c55a0846276d5c
mliu95 <michael.liu95@gmail.com>
2014-03-17 11:25:37 -0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bufferheyo::Application.config.secret_key_base = '44251fc18d3ce4ddcc0cd9004b5a29f5e48740231b00a741970ee05c30ef2fc96e09b588269d729cd04f3aa0439493184e5c499e0f702fb9085a63d7eecd36ac'
