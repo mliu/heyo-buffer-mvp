2c423183825d1fc230aaefc18f28b3473cff8a52
mliu95 <michael.liu95@gmail.com>
2014-03-21 09:45:26 -0400

    realized all times are in UTC

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   before_filter :require_login
-  around_filter :user_time_zone, :if => :current_user
 
-  def user_time_zone(&block)
-    Time.use_zone(current_user.time_zone, &block)
-  end
 
   private
     def current_user
--- db/migrate/20140321133916_add_time_zone_to_users.rb
+++
@@ -1,4 +0,0 @@
-class AddTimeZoneToUsers < ActiveRecord::Migration
-  def change
-  end
-end
